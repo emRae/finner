9483129ad8fb04299be4dea864df368dd1356d28
Eugene Kim <eugene.edward.kim@gmail.com>
2017-03-09 20:51:48 -0700

    pushing changes on auth

--- client/components/DietGoals.js
+++ client/components/DietGoals.js
@@ -3,21 +3,15 @@ import { connect } from 'react-redux';
 import { refreshLogin } from '../actions/auth';
 import { setFlash } from '../actions/flash';
 
-class Auth extends React.Component {
+class DietGoals extends React.Component {
   handleSubmit = (e) => {
     e.preventDefault();
-    let { email, password, weight, height, age, sex, props: { location, dispatch, router }} = this;
+    let { email, password, props: { location, dispatch, router }} = this;
 
     $.ajax({
       url: `/api/auth/${location.pathname}`,
       type: 'POST',
-      data: { email: email.value, 
-        password: password.value, 
-        weight: weight.value, 
-        height: height.value, 
-        age: age.value, 
-        sex: sex.value 
-      }
+      data: { email: email.value, password: password.value }
     }).done( user => {
       dispatch(refreshLogin(user));
       router.push("/")
@@ -38,11 +32,11 @@ class Auth extends React.Component {
             <input type="number" required={true} ref={n => this.age =n } placeholder='Age'/>
             <p>
             <input type="radio" required={true} name='male' ref={n => this.sex =n } id='male'/>
-              <label id='male'>Male</label>
+              <label for='male'>Male</label>
             </p>
             <p>
             <input type="radio" required={true} name='female' ref={n => this.sex =n } id='female' />
-              <label id='female'>Female</label>
+              <label for='female'>Female</label>
             </p>
               <br/>
            <button className="btn center">{this.props.route.title}</button>
@@ -52,4 +46,4 @@ class Auth extends React.Component {
   }
 }
 
-export default connect()(Auth);
+export default connect()(DietGoals);
