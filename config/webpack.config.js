bcffb4e8a2872916a1a17c91c7b0ef0366f2f259
Eugene Kim <eugene.edward.kim@gmail.com>
2017-03-14 19:39:25 -0600

    fixed webpack again

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -29,7 +29,10 @@ module.exports = {
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
         loader: 'url-loader',
-        include: [ path.join(__dirname, "..", "client") ],
+        include: [ path.join(__dirname, "..", "client")],
+        options: {
+        limit: 25000
+        }
       },
       { 
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
