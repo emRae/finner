e6ff872e3cd6d854f06d2d8d7e2e93774b1fee0c
Eugene Kim <eugene.edward.kim@gmail.com>
2017-04-03 20:11:28 -0600

    updated meals to use forms

--- client/components/Meals.js
+++ client/components/Meals.js
@@ -5,54 +5,38 @@ import unirest from 'unirest';
 import Meal from './Meal';
 
 class Meals extends React.Component {
-  state = {data:{results:[]}}
-  
-  // componentDidMount() {
-  //     $.ajax({
-  //       type: 'GET',
-  //       headers: {
-  //           'X-Mashape-Key': 'nP6VWgqTHxmshBXx1YQWWI9WHzSJp1ADIYOjsndhN7Zw2hjyAS'
-  //       },
-  //       url: 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/searchComplex?addRecipeInformation=false&cuisine=american&excludeIngredients=coconut%2C+mango&fillIngredients=false&includeIngredients=onions%2C+lettuce%2C+tomato&instructionsRequired=false&intolerances=peanut%2C+shellfish&limitLicense=false&maxCalories=1500&maxCarbs=100&maxFat=100&maxProtein=100&minCalories=150&minCarbs=5&minFat=5&minProtein=5&number=10&offset=0&query=burger&ranking=1&type=main+course',
-  //     }).done(data => {
-  //         console.log("ok")
-  //         this.setState({data: {results: data.results}}, () => {
-          
-  //       });
-  //     });
-  // }
+  state = {data:{results:[], cuisine: '', query: '', number: '', type:''}}
 
   handleSubmit = (e) => {
     e.preventDefault();
-    let recipe = 'false';
-    let cuisine = 'korean';
-    let excludeIngredients = '';
+    let recipe = 'true';
+    let cuisine = this.state.cuisine;
+    let excludeIngredients = 'peanut';
     let fillIngredients = 'false';
     let includeIngredients = 'beef';
     let instructionsRequired = 'false';
     let intolerances = '';
     let limitLicense = 'false';
-    let maxCalories = 1500;
+    let maxCalories = 500;
     let maxCarbs = 200;
     let maxFat = 100;
     let maxProtein = 100;
-    let minCalories = 150;
-    let minCarbs = 15;
-    let minFat = 5;
-    let minProtein = 5;
-    let number = 5;
+    let minCalories = 300;
+    let minCarbs = 150;
+    let minFat = 100;
+    let minProtein = 100;
+    let number = this.state.number;
     let offset = 0;
-    let query = 'beef';
+    let query = this.state.query;
     let ranking = 1;
-    let type = 'main course';
+    let type = this.state.type;
 
       $.ajax({
         type: 'GET',
         headers: {
             'X-Mashape-Key': 'nP6VWgqTHxmshBXx1YQWWI9WHzSJp1ADIYOjsndhN7Zw2hjyAS'
         },
-        url: `https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/searchComplex?${recipe}&${cuisine}&${instructionsRequired}&${excludeIngredients}&${fillIngredients}&${includeIngredients}&${instructionsRequired}&${intolerances}&${limitLicense}&${maxCalories}&${maxCarbs}&${maxFat}&${maxProtein}&${minCalories}&${minCarbs}&${minFat}&${minProtein}&${number}&${offset}&${query}&${ranking}&${type}`,
-        // url: 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/searchComplex?addRecipeInformation=false&cuisine=korean&instructionsRequired=false&limitLicense=false&maxCalories=1500&maxCarbs=100&maxFat=100&maxProtein=100&minCalories=150&minCarbs=5&minFat=5&minProtein=5&number=10&offset=0&query=beef&ranking=1'
+        url: `https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/searchComplex?addRecipeInformation=${recipe}&cuisine=${cuisine}&excludeIngredients=${excludeIngredients}&limitLicense=${limitLicense}&maxCalories=${maxCalories}&maxCarbs=${maxCarbs}&maxFat=${maxFat}&$maxProtein=${maxProtein}&minCalories=${maxCalories}&minCarbs=${maxCarbs}&minFat=${maxFat}&$minProtein=${maxProtein}&number=${number}&offset=${offset}&query=${query}&ranking=${ranking}`,
       }).done(data => {
           console.log("click")
           this.setState({data: {results: data.results}}, () => {
@@ -60,13 +44,9 @@ class Meals extends React.Component {
       });
   }
 
-  // recipes = () => {
-  //   this.state.data.results.map( recipe => {
-  //     return(
-  //         <div>{recipe.title}</div>
-  //     )
-  //   })
-  // }
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
 
   render() {
     console.log(this.state.data.results)
@@ -79,7 +59,26 @@ class Meals extends React.Component {
       <div>
           <h2 className="center">Your {this.props.route.title} for Today Are:</h2>
             <div>{recipes}</div>
-            <button className="btn center" onClick={this.handleSubmit}>Meals</button>
+            <form onSubmit={this.handleSubmit}>
+              <p>What would you like to eat today?</p>
+              <input type="text" name='cuisine' onChange={this.handleChange} placeholder="Chinese, American, Ethiopian?" />
+              <p>How many choices do you want?</p>
+              <input type="number" name='number' onChange={this.handleChange} placeholder="5" />
+              <p>What do you want to include?</p>
+              <input type="text" required={true} name='query' onChange={this.handleChange} placeholder="Beef, chicken, or pees?" />
+              <p>What's this for?</p>
+              <input type="radio" value="breakfast" required={true} onChange={this.handleChange} name='type' id='breakfast'/>
+                <label htmlFor='breakfast'>Breakfast</label>
+              <input type="radio" value="lunch"required={true} onChange={this.handleChange} name='type' id='lunch' />
+                <label htmlFor='lunch'>Lunch</label>
+              <input type="radio" value="dinner"required={true} onChange={this.handleChange} name='type' id='dinner'/>
+                <label htmlFor='dinner'>Dinner</label>
+              <input type="radio" value="snack"required={true} onChange={this.handleChange} name='type' id='snack' />
+              <label htmlFor='snack'>Snack</label>
+              <br/>
+              <br/>
+            <button className='btn center'>Get My Meal</button>
+            </form>
         </div>
     )
   }
