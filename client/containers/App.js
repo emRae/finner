6c9b7f6c95cdaba109ff5fb2f86ad8982b5a45e6
Rachel Engle <u1007081@Rachel-E.local>
2017-04-03 20:34:40 -0600

    few tweaks with home route

--- client/containers/App.js
+++ client/containers/App.js
@@ -55,7 +55,7 @@ class App extends React.Component {
       return links;
     } else {
       return [
-        { name: 'Home', path: '/home' },
+        { name: 'Home', path: '/' },
         { name: 'Sign In', path: '/signin' },
         { name: 'Sign Up', path: '/signup' },
       ].map( (link, i) => {
